058985f90481902e31a3dfe4433395261c60dfa9
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2024-11-17 07:35:36 -0700

    properly listing items

--- src/main.c
+++ src/main.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define MAX_NODES 8
+#define MAX_NODES 64
 
 typedef struct {
   char *name;
@@ -34,23 +35,26 @@ struct Table {
 };
 
 int rows, cols;
-int row_position, col_position;
+int rows_limit;
+int row_position = 0, col_position = 0;
+int buffer_pos = 0;
 int current_node_key = 0;
 
 size_t hash(size_t key);
+void elipsize(char *elipsis_buf, const char *original, const size_t max_size);
+const char *type_to_string(unsigned char type);
 int create_node(Table *ptr, size_t key);
 void table_set_buffer(Table *table, size_t key, DirectoryInfo *dbuf);
 Node *search_table(Table *ptr, size_t key);
 void err_callback(const char *prefix, const char *string);
 void print_callback(const char *prefix, const char *string);
 int no_hidden_path(const char *string);
-void list_draw(DirectoryInfo *buf, struct _win_st *win);
+void list_draw(DirectoryInfo *buf);
 const int *position_clamp(int *pos, int max);
 DirectoryInfo *search_directory(const char *path, const size_t path_len);
-void free_filesys_buffer(DirectoryInfo *buf, const int *ignore);
+void free_filesys_buffer(DirectoryInfo *buf);
 
 int main(int argc, char **argv) {
-
   FILE *stderr_file = freopen("errlog.txt", "a", stderr);
   if (!stderr_file) {
     err_callback("freopen failed!", strerror(errno));
@@ -102,12 +106,27 @@ int main(int argc, char **argv) {
 
   // expands to row = getmaxy, col = getmaxx()
   getmaxyx(win, rows, cols);
-  list_draw(search_table(table, current_node_key)->info_ptr, win);
+  list_draw(search_table(table, current_node_key)->info_ptr);
   move(0, 0);
 
   int running = 1;
   while (running) {
+
+    getmaxyx(win, rows, cols);
+    rows_limit = rows * 0.75;
+
+    Node *current_node = search_table(table, current_node_key);
+    const size_t list_size = current_node->info_ptr->total_size;
+
+    if (rows_limit > list_size) {
+      rows_limit = list_size;
+    }
+
+    list_draw(current_node->info_ptr);
+    move(row_position, col_position);
+
     const int ch = getch();
+
     switch (ch) {
 
     case 'q': {
@@ -124,35 +143,67 @@ int main(int argc, char **argv) {
       } break;
 
       case DT_DIR: {
-
         Node *current = search_table(table, current_node_key);
         // I don't need to worry about accessing outside bounds here since the
         // row_position index is always clamped to the size of the buffer.
-        search_buffer = current->info_ptr[row_position].path_str;
-        const size_t length = current->info_ptr[row_position].path_length;
+        search_buffer = current->info_ptr[buffer_pos].path_str;
+        const size_t length = current->info_ptr[buffer_pos].path_length;
+
         current_node_key++;
         position_clamp(&current_node_key, MAX_NODES);
-        table_set_buffer(table, current_node_key,
-                         search_directory(search_buffer, length));
-        list_draw(search_table(table, current_node_key)->info_ptr, win);
+        if (current_node_key != 0) {
+          free_filesys_buffer(search_table(table, current_node_key)->info_ptr);
+          table_set_buffer(table, current_node_key,
+                           search_directory(search_buffer, length));
+          row_position = 0;
+          buffer_pos = 0;
+        }
       } break;
+      }
+    } break;
+
+    case KEY_LEFT: {
+      const int tmp_key = current_node_key;
+      current_node_key--;
+      position_clamp(&current_node_key, MAX_NODES);
+
+      if (search_table(table, current_node_key)->info_ptr != NULL) {
+        row_position = 0;
+        buffer_pos = 0;
+      } else {
+        current_node_key = tmp_key;
+      }
+    } break;
+
+    case KEY_RIGHT: {
+      const int tmp_key = current_node_key;
+      current_node_key++;
+      position_clamp(&current_node_key, MAX_NODES);
+
+      if (search_table(table, current_node_key)->info_ptr != NULL) {
+        row_position = 0;
+        buffer_pos = 0;
+      } else {
+        current_node_key = tmp_key;
       }
     } break;
 
     case KEY_UP: {
       row_position--;
-      move(*position_clamp(
-               &row_position,
-               search_table(table, current_node_key)->info_ptr->total_size),
-           col_position);
+      buffer_pos--;
+      position_clamp(&row_position, rows_limit);
+      position_clamp(
+          &buffer_pos,
+          search_table(table, current_node_key)->info_ptr->total_size);
     } break;
 
     case KEY_DOWN: {
       row_position++;
-      move(*position_clamp(
-               &row_position,
-               search_table(table, current_node_key)->info_ptr->total_size),
-           col_position);
+      buffer_pos++;
+      position_clamp(&row_position, rows_limit);
+      position_clamp(
+          &buffer_pos,
+          search_table(table, current_node_key)->info_ptr->total_size);
     } break;
     }
 
@@ -168,30 +219,65 @@ int main(int argc, char **argv) {
 }
 
 const int *position_clamp(int *pos, int max) {
-  if (*pos > max - 1) {
+  if (max == 0) {
     *pos = 0;
     return pos;
   }
 
-  if (*pos < 0) {
+  if (*pos > max - 1) {
     *pos = max - 1;
+    return pos;
+  }
+
+  if (*pos < 0) {
+    *pos = 0;
     return pos;
   }
 
   return pos;
 }
 
-void list_draw(DirectoryInfo *buf, struct _win_st *win) {
-  wclear(win);
-  for (int i = 0; i < rows; i++) {
-    if (buf && i < (int)buf->total_size) {
-      if (buf[i].name) {
-        mvprintw(i, 0, "%s -> %d", buf[i].name, buf[i].type);
+void elipsize(char *elipsis_buffer, const char *original, size_t max_size) {
+  for (size_t i = 0; i < max_size; i++) {
+    elipsis_buffer[i] = original[i];
+  }
+
+  elipsis_buffer[max_size - 1] = '~';
+  elipsis_buffer[max_size] = '\0';
+}
+
+void list_draw(DirectoryInfo *buf) {
+
+  clear();
+  if (buf && buf->total_size == 0) {
+    return;
+  }
+
+  int j = 0;
+  if (buffer_pos >= rows_limit) {
+    j = buffer_pos - (rows_limit - 1);
+  }
+
+  const int left = cols - (cols - 2);
+
+  for (int i = j, k = 0; i < (int)buf->total_size && k < rows_limit; i++, k++) {
+    if (buf[i].name) {
+      const size_t max_size = 50;
+      char *name_ptr = NULL;
+      if (strlen(buf[i].name) >= max_size) {
+        char tmp_name_buf[max_size * 2];
+        elipsize(tmp_name_buf, buf[i].name, max_size);
+        name_ptr = tmp_name_buf;
+      } else {
+        name_ptr = buf[i].name;
       }
-    } else {
-      break;
+
+      mvprintw(k, left, "%s - (%s)", name_ptr, type_to_string(buf[i].type));
     }
   }
+
+  mvprintw(rows - (rows * 0.1), left, "%d/%zu", buffer_pos + 1,
+           buf->total_size);
 }
 
 DirectoryInfo *search_directory(const char *search_path,
@@ -275,30 +361,25 @@ DirectoryInfo *search_directory(const char *search_path,
   return buf;
 }
 
-void free_filesys_buffer(DirectoryInfo *buf, const int *ignore) {
+void free_filesys_buffer(DirectoryInfo *buf) {
+  if (!buf) {
+    return;
+  }
+
   for (int i = 0; i < (int)buf->total_size; i++) {
-    if (buf && ignore) {
-      if (buf[i].name && i != *ignore) {
-        free(buf[i].name);
-        buf[i].name = NULL;
-      }
+    if (buf[i].name) {
+      free(buf[i].name);
+      buf[i].name = NULL;
+    }
 
-      if (buf[i].path_str && i != *ignore) {
-        free(buf[i].path_str);
-        buf[i].path_str = NULL;
-      }
-    } else if (buf) {
-      if (buf[i].name) {
-        free(buf[i].name);
-        buf[i].name = NULL;
-      }
-
-      if (buf[i].path_str) {
-        free(buf[i].path_str);
-        buf[i].path_str = NULL;
-      }
+    if (buf[i].path_str) {
+      free(buf[i].path_str);
+      buf[i].path_str = NULL;
     }
   }
+
+  free(buf);
+  buf = NULL;
 }
 
 int no_hidden_path(const char *string) {
@@ -354,4 +435,18 @@ size_t hash(size_t key) { return key % MAX_NODES; }
 void table_set_buffer(Table *table, size_t key, DirectoryInfo *dbuf) {
   Node *node = search_table(table, key);
   node->info_ptr = dbuf;
+}
+
+const char *type_to_string(unsigned char type) {
+  switch (type) {
+  default: {
+    return "Unknown type";
+  } break;
+  case DT_REG: {
+    return "Regular file";
+  };
+  case DT_DIR: {
+    return "Directory";
+  } break;
+  }
 }
